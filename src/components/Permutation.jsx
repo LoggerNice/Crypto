bc1a51d669da365476968003be96bdb9ef8c1a7b
LoggerNice <sk27012005@mail.ru>
2022-10-11 21:11:41 +0400

    1

--- src/components/Permutation.jsx
+++ src/components/Permutation.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import _ from "lodash";
 
 function Permutation() {
   const [messageValue, setmessageValue] = useState('')
@@ -11,26 +12,47 @@ function Permutation() {
   var rows = []
   let row = ''
 
-  //wordCode записывать побуквенно. Исправить
   message.map((word, idx) => {
     row += word
     if((idx + 1) % countRow === 0) {
-      rows.push({word:wordCode, row:row})
+      let idxWord = Math.floor(idx / countRow)
+      rows.push({
+        word:wordCode[idxWord], 
+        row:row
+      })
       row = ''
     }
   })
 
-  console.log(rows)
+  var sortRows = _.sortBy(rows, ['word'])
+  var onlyRows = []
+  sortRows.map((item) => {
+    onlyRows.push(item['row'].split(''))
+  })
+
+  let hash = ''
+  for (let i = 0; i < countRow; i++) {
+    onlyRows.map((item, idx) => {
+      hash += item[i]
+      if((idx + 1) % countRow === 0) {
+        hash += ' '
+      }
+    })
+  }
 
   return (
     <div className='px-16'>
       <div class="relative z-0 mb-6 w-full group">
+          <input value={codeValue} onChange={e => setcodeValue(e.target.value)} type="text" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+          <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Введите ключ</label>
+      </div>
+      <div class="relative z-0 mb-6 w-full group">
           <input value={messageValue} onChange={e => setmessageValue(e.target.value)} type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
           <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Введите сообщение</label>
       </div>
-      <div class="relative z-0 mb-6 w-full group">
-          <input value={codeValue} onChange={e => setcodeValue(e.target.value)} type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-          <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Введите ключ</label>
+      <div class={hash.length <= 0 ? "hidden" : "relative z-0 mb-6 w-full group"}>
+          <input defaultValue={hash} type="text" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required readOnly/>
+          <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Шифртекст</label>
       </div>
     </div>
   )
